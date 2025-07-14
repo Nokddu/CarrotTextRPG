390e78beae5d391c5ece63612553dda9fc973013
euny0ng <h_lyn@hanmail.net>
2025-07-14 21:58:31 +0900

    creat UI

---
+++ Util.cs
@@ -0,0 +1,15 @@
+namespace CarrotTextRPG
+{
+       internal static class Util
+    {
+        public static void PrintLogo(string text)
+        {
+            Console.WriteLine("___________                                    .___       .__                     .___");
+            Console.WriteLine("\\_   _____/ ______ ____ _____  ______   ____   |   | _____|  | _____    ____    __| _/");
+            Console.WriteLine(" |    __)_ /  ___// ___\\\\__  \\ \\____ \\_/ ___\\  |   |/  _ \\  | \\__  \\ _/ ___\\  / __ | ");
+            Console.WriteLine(" |        \\\\___ \\\\  \\___ / __ \\|  |_> >\\  \\___  |   (  <_> )  |__/ __ \\\\  \\___ / / /_/ | ")
+            Console.WriteLine("/_______  /____  >\\___  >____  /   __/  \\___  > |___|\\____/|____(____  / \\___  > \\____ | ");
+            Console.WriteLine("        \\/     \\/     \\/     \\/|__|         \\/                     \\/      \\/       \\/ ");
+        }
+    }   
+}
